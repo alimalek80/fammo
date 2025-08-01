bd534a9229f2184640a2c63915512e8b417cb2a2
alimalek <alianta2016@gmail.com>
2025-08-01 23:40:37 +0300

    Modify Pets Cards in mobile view

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home, manage_hero_section, manage_social_links
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),
     path('dashboard/hero-section/', manage_hero_section, name='manage_hero_section'),
     path('dashboard/social-links/', manage_social_links, name='manage_social_links'),
+    path('about/', TemplateView.as_view(template_name='core/about.html'), name='about'),
 ]
