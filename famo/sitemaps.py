c3ca38458c9e1af1cfedb295391401bd3ea84043
Ali Malek <alianta2016@gmail.com>
2026-04-30 13:06:52 +0300

    Simplify sitemap to avoid 500 errors - use direct paths

--- famo/sitemaps.py
+++ famo/sitemaps.py
@@ -7,85 +7,87 @@ class StaticViewSitemap(Sitemap):
     """Sitemap for static pages"""
     priority = 0.8
     changefreq = 'weekly'
-    i18n = True  # Enable i18n support
+    protocol = 'https'
 
     def items(self):
-        # Return list of static page URL names (without namespace for simplicity)
+        # Return simple paths
         return [
-            'home', 'about', 'how_it_works', 'how_fammo_works', 'contact', 'faq'
+            '/',
+            '/about/',
+            '/contact/',
+            '/faq/',
+            '/blog/',
+            '/forum/',
+            '/vets/clinics/',
         ]
 
     def location(self, item):
-        # Reverse with core namespace
-        try:
-            return reverse(f'core:{item}')
-        except:
-            return f'/{item}/'
+        return item
 
 
 class BlogPostSitemap(Sitemap):
     """Sitemap for published blog posts"""
     changefreq = 'weekly'
     priority = 0.9
-    i18n = True
+    protocol = 'https'
 
     def items(self):
-        from blog.models import BlogPost
-        return BlogPost.objects.filter(
-            is_published=True,
-            published_at__isnull=False,
-            published_at__lte=timezone.now()
-        ).order_by('-published_at')
+        try:
+            from blog.models import BlogPost
+            return BlogPost.objects.filter(
+                is_published=True,
+                published_at__isnull=False,
+                published_at__lte=timezone.now()
+            ).order_by('-published_at')
+        except Exception:
+            return []
 
     def lastmod(self, obj):
         return obj.updated_at
 
     def location(self, obj):
-        try:
-            return reverse('blog:blog_detail', kwargs={'slug': obj.slug})
-        except:
-            return f'/blog/{obj.slug}/'
+        return f'/blog/{obj.slug}/'
 
 
 class ForumQuestionSitemap(Sitemap):
     """Sitemap for forum questions"""
     changefreq = 'daily'
     priority = 0.7
-    i18n = True
+    protocol = 'https'
 
     def items(self):
-        from forum.models import Question
-        return Question.objects.all().order_by('-created_at')[:100]  # Limit to latest 100
+        try:
+            from forum.models import Question
+            return Question.objects.all().order_by('-created_at')[:100]
+        except Exception:
+            return []
 
     def lastmod(self, obj):
         return obj.updated_at
 
     def location(self, obj):
-        try:
-            return reverse('forum:question_detail', kwargs={'pk': obj.pk})
-        except:
-            return f'/forum/question/{obj.pk}/'
+        return f'/forum/question/{obj.pk}/'
 
 
 class ClinicSitemap(Sitemap):
     """Sitemap for verified partner clinics"""
     changefreq = 'weekly'
     priority = 0.8
-    i18n = True
+    protocol = 'https'
 
     def items(self):
-        from vets.models import Clinic
-        return Clinic.objects.filter(
-            email_confirmed=True,
-            admin_approved=True,
-            is_verified=True
-        ).order_by('-created_at')
+        try:
+            from vets.models import Clinic
+            return Clinic.objects.filter(
+                email_confirmed=True,
+                admin_approved=True,
+                is_verified=True
+            ).order_by('-created_at')
+        except Exception:
+            return []
 
     def lastmod(self, obj):
         return obj.updated_at
 
     def location(self, obj):
-        try:
-            return reverse('vets:clinic_detail', kwargs={'slug': obj.slug})
-        except:
-            return f'/vets/clinic/{obj.slug}/'
+        return f'/vets/clinic/{obj.slug}/'
